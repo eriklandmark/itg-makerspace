b68f57d8d9b8f0ee23f547170cf99eeb1011000c
itggot-erik-landmark <erik.landmark@itggot.se>
2017-01-28 23:04:09 +0100

    Added first view

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,16 @@
 /**
  * Created by eriklandmark on 2017-01-27.
  */
+window.addEventListener("mousemove", movepictures, false);
+function movepictures(e) {
+    parralax(e, "content", 0.02);
+    parralax(e, "pi_logo", 0.05);
+    parralax(e, "arduino_logo", 0.09);
+    parralax(e, "itg_logo", 0.04);
+    parralax(e, "printer", 0.03);
+}
+function parralax(e, element, friction) {
+    var posX = (e.clientX * -friction) / 2.0;
+    var posY = (e.clientY * -friction) / 2.0;
+    document.getElementById(element).style.transform = 'translate(' + posX + 'px, ' + posY + 'px)';
+}
